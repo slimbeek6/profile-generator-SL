17e54f82c61f66edeec2628938590488c28fc6fd
Shaun Limbeek <soccercomet@yahoo.com>
2021-01-23 13:06:48 -0800

    copied over engineer to manager to create manager instantiator, updated with relevant necessary information

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("../lib/employee");
+
+class Manager extends Employee {
+    constructor(name, counta, emailAdd, officeNumber) {
+        const nameInt = name;
+        const offNum = officeNumber;
+        const email = emailAdd;
+        const role = "Manager";
+        const ID = counta;
+
+        super (nameInt, email, role, ID);
+        this.officeNumber = offNum;
+    }
+
+    getRole() {
+        return this.role;
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+}
+
+module.exports = Manager;
