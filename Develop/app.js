da3d0d2c5724d5eace167a29efe5a4203833f9c8
Shaun Limbeek <soccercomet@yahoo.com>
2021-01-23 14:35:24 -0800

    finished all inquirer prompts and connections, need to now properly tie to the renderer

--- Develop/app.js
+++ Develop/app.js
@@ -14,147 +14,9 @@ const employees =[];
 const memberQuest = ["Engineer","Intern","I don't want to enter another employee"];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-class TeamBuilder {
-    constructor() {
-        this.teamsize = 0;
-    }
+function App(){
 
-    setManager() {
-        inquirer
-            .prompt([
-                {
-                    type: "input",
-                    name: "nameMan",
-                    message: "What is the team manager's name?"
-                },
-                {
-                    type: "input",
-                    name: "ID",
-                    message: "What is the team manager's id?"
-                },
-                {
-                    type: "email",
-                    name: "email",
-                    message: "What is the team manager's email?",
-                },
-                {
-                    type: "input",
-                    name: "officeNum",
-                    message: "What is the team manager's office number?"
-                }
-            ])
-            .then(data => {
-                var name = data.nameMan;
-                var ID = data.ID;
-                var emailMan = data.email;
-                var offNum = data.officeNum;
-                var manager = new Manager (name, ID, emailMan, offNum);
-                employees.push(manager);
-            });
-            this.askTeam();
-    }
-
-    askTeam() {
-        inquirer
-            .prompt([
-                {
-                    type: "list",
-                    name: "employeeType",
-                    message: "Which type of team member would you like to add?",
-                    choices: memberQuest
-                }
-            ])
-            .then(data => {
-                if (data.employeeType === "Engineer") {
-                    setEngineer();
-                }
-                else if (data.employeeType === "Intern") {
-                    setIntern();
-                }
-                else {
-                    render();
-                    return;
-                }
-            });
-    }
-
-    setEngineer() {
-        inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "nameEng",
-                message: "What is this engineer's name?"
-            },
-            {
-                type: "input",
-                name: "ID",
-                message: "What is this engineer's id?"
-            },
-            {
-                type: "email",
-                name: "email",
-                message: "What is this engineers's email?",
-            },
-            {
-                type: "input",
-                name: "github",
-                message: "What is this engineer's github userID?"
-            }
-        ])
-        .then(data => {
-            var name = data.nameEng;
-            var engId = data.ID;
-            var engEmail = data.email;
-            var engGit = data.github;     
-            var engg = new Engineer(name, engId, engEmail, engGit);
-            employees.push(engg);
-        });
-        this.askTeam();
-    }
-
-    setIntern() {
-        inquirer
-        .prompt ([
-            {
-                type: "input",
-                name: "nameInt",
-                message: "What is this intern's name?"
-            },
-            {
-                type: "input",
-                name: "ID",
-                message: "What is this intern's id?"
-            },
-            {
-                type: "email",
-                name: "email",
-                message: "What is this intern's email?",
-            },
-            {
-                type: "input",
-                name: "school",
-                message: "Where does this intern go to school?"
-            }
-        ])
-        .then(data => {
-            var name = data.nameInt;
-            var intId = data.ID;
-            var intEmail = data.email;
-            var school = data.school;     
-            var intern = new Intern(name, intId, intEmail, school);
-            employees.push(intern);
-        });
-        this.askTeam();
-
-    }
-
-
-
-
-
-
-
+    setManager(); 
 
 }
 // After the user has input all employees desired, call the `render` function (required
@@ -176,3 +38,144 @@ class TeamBuilder {
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+function askTeam() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "employeeType",
+                message: "Which type of team member would you like to add?",
+                choices: memberQuest
+            }
+        ])
+        .then(data => {
+            if (data.employeeType === "Engineer") {
+                setEngineer();
+            }
+            else if (data.employeeType === "Intern") {
+                setIntern();
+            }
+            else {
+                render();
+                return;
+            }
+        });
+}
+
+
+function setManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "nameMan",
+                message: "What is the team manager's name?"
+            },
+            {
+                type: "input",
+                name: "ID",
+                message: "What is the team manager's id?"
+            },
+            {
+                type: "email",
+                name: "email",
+                message: "What is the team manager's email?",
+            },
+            {
+                type: "input",
+                name: "officeNum",
+                message: "What is the team manager's office number?"
+            }
+        ])
+        .then(data => {
+            var name = data.nameMan;
+            var ID = data.ID;
+            var emailMan = data.email;
+            var offNum = data.officeNum;
+            var manager = new Manager (name, ID, emailMan, offNum);
+            employees.push(manager);
+            // console.log(employees);
+            askTeam();
+        });
+        
+}
+
+
+
+function setIntern()  {
+    inquirer
+    .prompt ([
+        {
+            type: "input",
+            name: "nameInt",
+            message: "What is this intern's name?"
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "What is this intern's id?"
+        },
+        {
+            type: "email",
+            name: "email",
+            message: "What is this intern's email?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Where does this intern go to school?"
+        }
+    ])
+    .then(data => {
+        var name = data.nameInt;
+        var intId = data.ID;
+        var intEmail = data.email;
+        var school = data.school;     
+        var intern = new Intern(name, intId, intEmail, school);
+        employees.push(intern);
+        askTeam();
+    });
+    
+
+}
+
+function setEngineer () {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "nameEng",
+            message: "What is this engineer's name?"
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "What is this engineer's id?"
+        },
+        {
+            type: "email",
+            name: "email",
+            message: "What is this engineers's email?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is this engineer's github userID?"
+        }
+    ])
+    .then(data => {
+        var name = data.nameEng;
+        var engId = data.ID;
+        var engEmail = data.email;
+        var engGit = data.github;     
+        var engg = new Engineer(name, engId, engEmail, engGit);
+        employees.push(engg);
+        console.log(employees);
+        askTeam();
+    });
+    
+}
+
+App();
+
+module.exports = App;
