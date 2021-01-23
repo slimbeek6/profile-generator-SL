253aa9c1b93bae3e9ebb610d4834d0221714ec96
Shaun Limbeek <soccercomet@yahoo.com>
2021-01-23 12:56:17 -0800

    Created Employee class and intern class. Initially had intern class working, but not as a subclass of employee

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor (name, email, role, ID) {
+        this.name = name;
+        this.ID = 1;
+        this.email = email;
+        this.role = role;
+        this.count = count;
+    }
+
+
+}
+
+module.exports = Employee;
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("../lib/employee");
+
+class Intern extends Employee {
+    constructor(name, counta, emailAdd, school) {
+        const nameInt = name;
+        const schoolInt = school;
+        const email = emailAdd;
+        const role = "Intern";
+        const count = counta;
+
+        super (nameInt, email, role, ID);
+        this.school = schoolInt;
+    }
+
+    getRole() {
+        return this.role;
+    }
+
+    getSchool() {
+        return this.school;
+    }
+}
+
+module.exports = Intern;
